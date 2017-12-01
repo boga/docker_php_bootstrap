6db129d02786851a60280956d7422f49dde28441
Ivan Shnurchenko <ivan.shnurchenko@gmail.com>
2017-12-01 13:32:42 +0300

    Added DB connection checking

--- php-code/index.php
+++ php-code/index.php
@@ -1,2 +1,9 @@
 <?
+try {
+    $db = new PDO("mysql:host=db;dbname=docker", "docker", "docker");
+    echo("DB connect OK");
+} catch (PDOException $e) {
+    echo("DB connect failed: " . $e->getMessage());
+}
+
 phpinfo();
