f4c7e5415b1a4f837f1432a0a5be2b273d434cca
Ivan Shnurchenko <ivan.shnurchenko@gmail.com>
2018-08-14 23:32:53 +0300

    Renamed test files

--- php-code/index.php
+++
@@ -1,9 +0,0 @@
-<?
-try {
-    $db = new PDO("mysql:host=db;dbname=docker", "docker", "docker");
-    echo("DB connect OK");
-} catch (PDOException $e) {
-    echo("DB connect failed: " . $e->getMessage());
-}
-
-phpinfo();
